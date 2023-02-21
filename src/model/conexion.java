323b5db9736c2583b8fc9c19320349c894bf2ab0
Andrés O <aforozco556340@umanizales.edu.co>
2023-02-20 20:42:24 -0500

    Initial commit

---
+++ src/model/conexion.java
@@ -0,0 +1,36 @@
+package model;
+
+public class conexion
+{
+    //Declaramos el objeto de prueba
+    private conexion objetoPrb;
+
+    //Declaramos el constructor de acceso privado para evitar que el objeto se instancie con el operador new
+    private conexion () {
+
+    }
+
+    /*Declaramos el metodo de acceso al constructor utilizando la palabra reservada "static" para hacer
+    posible el acceso mediante el clase.metodo
+     */
+    public static conexion getObjeto()
+    {
+        if(objetoPrb == null)
+        {
+            objetoPrb = new conexion();
+        }
+        return objetoPrb;
+    }
+
+    //Metodo de prueba simulando conexión a DB
+    public void conectar()
+    {
+        System.out.println("Me conecte a la bd");
+    }
+
+    //Metodo de prueba simulando desconexión de DB
+    public void desconectar()
+    {
+        System.out.println("Me desconecte de la BD");
+    }
+}
